e2b7a1082a163e0e4957352357491d0de4be729f
Marian Theisen <marian@cice-online.net>
2012-01-01 12:32:35 +0100

    refactored section helper

--- lib/brad/resources/view/layout_helpers/sections.rb
+++ lib/brad/resources/view/layout_helpers/sections.rb
@@ -1,21 +1,16 @@
 module Brad::Resources::View::LayoutHelpers
   module Sections
-    def section *args
+    def section *args, &block
       options = args.extract_options!
       h1, small = args
       
-      content_tag :section, options do
-        if h1
-          concat content_tag(:div, :class => 'page-header') {
-            concat content_tag(:h1) {
-              concat h1
-              small && concat(content_tag(:small, small))
-            }
-          }
-        end
-        
-        yield
-      end
+      locals = {
+        :options => options,
+        :h1 => h1,
+        :small => small
+      }
+      
+      render :layout => "bootstrap/layout/section", :locals => locals, &block
     end
   end
 end
