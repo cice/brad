6611765079517b8ff1d9f7088f65b5d9303938ac
Marian Theisen <marian@cice-online.net>
2012-01-07 19:34:34 +0100

    aligning hash

--- lib/brad/resources/view/layout_helpers/sections.rb
+++ lib/brad/resources/view/layout_helpers/sections.rb
@@ -5,9 +5,9 @@ module Brad::Resources::View::LayoutHelpers
       h1, small = args
       
       locals = {
-        :options => options,
-        :h1 => h1,
-        :small => small
+        :options  => options,
+        :h1       => h1,
+        :small    => small
       }
       
       render :layout => "bootstrap/layout/section", :locals => locals, &block
