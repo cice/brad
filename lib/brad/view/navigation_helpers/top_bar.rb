57d97d81362585b5f7297a08484593e68835cd6c
Marian Theisen <marian@cice-online.net>
2012-01-07 22:23:40 +0100

    nav list and link to

--- lib/brad/view/navigation_helpers/top_bar.rb
+++ lib/brad/view/navigation_helpers/top_bar.rb
@@ -10,5 +10,22 @@ module Brad::View::NavigationHelpers
       
       render :partial => 'bootstrap/navigation/top_bar', :locals => locals
     end
+    
+    def nav_list &block
+      render :layout => 'bootstrap/navigation/nav_list', &block
+    end
+    
+    def nav_link_to body_or_url, *link_to_args, &block
+      url = block_given? ? body_or_url : link_to_args.first
+      is_active = current_page?(url) ? 'active' : nil
+      
+      locals = {
+        :body       => body_or_url,
+        :args       => link_to_args,
+        :block      => block,
+        :li_class   => is_active
+      }
+      render :partial => 'bootstrap/navigation/nav_link_to', :locals => locals
+    end
   end
 end
--- spec/unit/resources/view/navigation_helpers/top_bar_spec.rb
+++ spec/unit/resources/view/navigation_helpers/top_bar_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe Brad::View::NavigationHelpers::TopBar, :type => :helper do
+  let(:current_page) do
+    '/buzz'
+  end
+  
+  described_class.class_eval do
+    define_method :current_page? do |options|
+      current_page == options
+    end
+  end
+  
   it 'should render basic scaffold' do
     buffer = helper.top_bar
     
@@ -11,5 +21,21 @@ describe Brad::View::NavigationHelpers::TopBar, :type => :helper do
     buffer = helper.top_bar 'foobar'
     
     buffer.should have_selector('.topbar .topbar-inner .container h3 a[href="#"]', :content => 'foobar')
+  end
+  
+  it 'should render brand with url' do
+    buffer = helper.top_bar 'foobar', '/baz'
+    
+    buffer.should have_selector('.topbar .topbar-inner .container h3 a[href="/baz"]')
+  end
+  
+  it 'should render a nav list' do
+    buffer = helper.nav_list do
+      concat nav_link_to('foo', '/baz')
+      concat nav_link_to('bar', '/buzz')
+    end
+    
+    buffer.should have_selector('ul.nav li a[href="/baz"]', :content => 'foo')
+    buffer.should have_selector('ul.nav li.active a[href="/buzz"]', :content => 'bar')
   end
 end
