7a41a85a65c1846dc16a64f76ade985e2a3f22e2
Marian Theisen <marian@cice-online.net>
2012-02-25 15:55:20 +0100

    more examples

--- lib/brad_views/icons.rb
+++ lib/brad_views/icons.rb
@@ -1,7 +1,10 @@
 module BradViews
   module Icons
-    def icon type, label = nil
-      content_tag(:i, '', :class => "icon-#{type}") + label.to_s
+    def icon type, *args
+      options = args.extract_tag_options!.merge! :class => "icon-#{type}"
+      label = args.first
+
+      content_tag(:i, '', options) + label.to_s
     end
   end
 end
